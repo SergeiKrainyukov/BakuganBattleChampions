e8330cab4d52852c256ab817a76e9e7db5192b92
Sergei <sk_developer@mail.ru>
2023-11-28 14:56:10 +0300

    Отрисован экран меню

--- app/src/main/java/com/animeGames/bakuganbattlechampions/MainActivity.kt
+++ app/src/main/java/com/animeGames/bakuganbattlechampions/MainActivity.kt
@@ -6,10 +6,11 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.animeGames.bakuganbattlechampions.ui.theme.BakuganBattleChampionsTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +23,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    App()
                 }
             }
         }
@@ -30,17 +31,16 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BakuganBattleChampionsTheme {
-        Greeting("Android")
+fun App() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "main") {
+        composable("main") { MenuScreen(navController) }
+        composable("campaign") { /* ... */ }
+        composable("collections") { /* ... */ }
+        composable("shop") { /* ... */ }
+        composable("settings") { /* ... */ }
+        composable("profile") { /* ... */ }
     }
 }
+
+
---
+++ app/src/main/java/com/animeGames/bakuganbattlechampions/MenuScreen.kt
@@ -0,0 +1,80 @@
+package com.animeGames.bakuganbattlechampions
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.animeGames.bakuganbattlechampions.ui.theme.BakuganBattleChampionsTheme
+
+@Composable
+fun MenuScreen(navController: NavController) {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            // Определяем общий модификатор для кнопок
+            val buttonModifier = Modifier
+                .fillMaxWidth() // Заполняем всю доступную ширину
+                .padding(horizontal = 16.dp) // Добавляем горизонтальный отступ
+
+            Button(
+                onClick = { navController.navigate("campaign") },
+                modifier = buttonModifier
+            ) {
+                Text("Кампания", fontSize = 22.sp)
+            }
+
+            Button(
+                onClick = { navController.navigate("collections") },
+                modifier = buttonModifier
+            ) {
+                Text("Коллекции", fontSize = 22.sp)
+            }
+
+            Button(
+                onClick = { navController.navigate("shop") },
+                modifier = buttonModifier
+            ) {
+                Text("Магазин", fontSize = 22.sp)
+            }
+
+            Button(
+                onClick = { navController.navigate("settings") },
+                modifier = buttonModifier
+            ) {
+                Text("Настройки", fontSize = 22.sp)
+            }
+
+            Button(
+                onClick = { navController.navigate("profile") },
+                modifier = buttonModifier
+            ) {
+                Text("Профиль", fontSize = 22.sp)
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MenuScreenPreview() {
+    BakuganBattleChampionsTheme {
+        MenuScreen(navController = rememberNavController())
+    }
+}
